2d84aa726ff8da6bcf62185ec20293a369924217
tahhh2234 <thadpongsermsook1@gmail.com>
2025-02-02 21:32:59 +0700

    setup project

---
+++ portfolio/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import Sitemap from 'vite-plugin-sitemap';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), Sitemap()],
+});
